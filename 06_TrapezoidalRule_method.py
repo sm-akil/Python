6ccd2bc5f373b44a08900cacb3e5bff2fd4feddc
ｓａｄｉｋ _ｉｑｂａｌ_ａｋｉｌ <69757433+sm-akil@users.noreply.github.com>
2022-09-24 10:10:47 +0600

    Create 06_TrapezoidalRule_method.py

---
+++ 06_TrapezoidalRule_method.py
@@ -0,0 +1,38 @@
+
+# Define function to integrate
+def f(x):
+    return 1/(1 + x**2)
+
+# Implementing trapezoidal method
+def trapezoidal(x0,xn,n):
+    # calculating step size
+    h = (xn - x0) / n
+    
+    # Finding sum 
+    integration = f(x0) + f(xn)
+    
+    for i in range(1,n):
+        k = x0 + i*h
+        integration = integration + 2 * f(k)
+    
+    # Finding final integration value
+    integration = integration * h/2
+    
+    return integration
+    
+# Input section
+lower_limit = float(input("Enter lower limit of integration: "))
+upper_limit = float(input("Enter upper limit of integration: "))
+sub_interval = int(input("Enter number of sub intervals: "))
+
+# Call trapezoidal() method and get result
+result = trapezoidal(lower_limit, upper_limit, sub_interval)
+print("Integration result by Trapezoidal method is: %0.6f" % (result) )
+
+# Trapezoidal Method ~ OUTPUT
+"""
+Enter lower limit of integration: 0
+Enter upper limit of integration: 1
+Enter number of sub intervals: 6
+Integration result by Trapezoidal method is: 0.784241
+"""
